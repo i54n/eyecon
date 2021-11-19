b733918d59e5459d92e5d8ed2c74879c59d693e7
i54n <56086253+i54n@users.noreply.github.com>
2021-11-19 23:29:26 +0700

    Update cssscript.com-simple-table-paginator-pure.js

--- cssscript.com-simple-table-paginator-pure.js
+++ cssscript.com-simple-table-paginator-pure.js
@@ -217,9 +217,9 @@ function paginator(config) {
     if (!(typeof config.page_options == "boolean" && !config.page_options)) {
         if (typeof config.page_options == "undefined") {
             config.page_options = [
-                { value: 5,  text: '5'   },
-                { value: 10, text: '10'  },
-                { value: 20, text: '20'  },
+                //{ value: 5,  text: '5'   },
+                //{ value: 10, text: '10'  },
+                //{ value: 20, text: '20'  },
                 { value: 50, text: '50'  },
                 { value: 100,text: '100' },
                 { value: 0,  text: 'All' }
